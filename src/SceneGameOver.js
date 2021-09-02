cb44c96fdad1edc9beeecc9279f459e8dc10efc2
Alexis Varela <alexisbec13@hotmail.com>
2021-09-01 20:38:19 -0500

    - Add functionality to SceneMainMane & SceneGameOver

--- src/SceneGameOver.js
+++ src/SceneGameOver.js
@@ -3,7 +3,36 @@ class SceneGameOver extends Phaser.Scene {
     super({ key: 'SceneGameOver' });
   }
 
+  preload() {
+    this.load.image('gameOver', '../dist/assets/gameOver.png');
+    this.load.image('restartBtn', '../dist/assets/restartBtn.png');
+    this.load.image('bg1', '../dist/assets/bg1.jpg');
+    this.load.image('bg2', '../dist/assets/bg2.png');
+  }
+
   create() {
-    
+    this.bg1 = this.add.tileSprite(300, 200, 1000, 580, 'bg1');
+    this.bg2 = this.add.tileSprite(300, 200, 1000, 580, 'bg2');
+
+    this.gameOver = this.add.image(this.game.config.width * 0.5, 120, 'gameOver');
+    this.gameOver.setOrigin(0.5);
+
+    this.restartBtn = this.add.sprite(
+      this.game.config.width * 0.5,
+      this.game.config.height - 170,
+      'restartBtn'
+    );
+
+    this.restartBtn.setInteractive();
+
+    this.restartBtn.on("pointerup", function() {
+      this.restartBtn.setTexture("restartBtn");
+      this.scene.start("SceneMain");
+    }, this);
+  }
+
+  update() {
+    this.bg1.tilePositionX += 2;
+    this.bg2.tilePositionX += 1;
   }
 }
--- src/SceneMainMenu.js
+++ src/SceneMainMenu.js
@@ -4,13 +4,35 @@ class SceneMainMenu extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('btnPlay', '../dist/assets/sprBtnPlay.png');
-    this.load.image('btnPlayDown', '../dist/assets/sprBtnPlayDown.png');
-    this.load.image('btnPlayOver', '../dist/assets/sprBtnPlayHover.png');
+    this.load.image('logo', '../dist/assets/logo.png');
+    this.load.image('startBtn', '../dist/assets/startBtn.png');
+    this.load.image('bg1', '../dist/assets/bg1.jpg');
+    this.load.image('bg2', '../dist/assets/bg2.png');
   }
 
   create() {
-    this.scene.start('SceneMain');
-    console.log('hello');
+    this.bg1 = this.add.tileSprite(300, 200, 1000, 580, 'bg1');
+    this.bg2 = this.add.tileSprite(300, 200, 1000, 580, 'bg2');
+
+    this.logo = this.add.image(this.game.config.width * 0.5, 120, 'logo');
+    this.logo.setOrigin(0.5);
+
+    this.startBtn = this.add.sprite(
+      this.game.config.width * 0.5,
+      this.game.config.height - 170,
+      "startBtn"
+    );
+
+    this.startBtn.setInteractive();
+
+    this.startBtn.on("pointerup", function() {
+      this.startBtn.setTexture("startBtn");
+      this.scene.start("SceneMain");
+    }, this);
+  }
+
+  update() {
+    this.bg1.tilePositionX += 2;
+    this.bg2.tilePositionX += 1;
   }
 }
